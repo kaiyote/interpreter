16004cfc69df1bd63e8ad7889fbbd6aa6c3da76b
Tim Huddle <blazhen88@gmail.com>
2017-04-10 19:07:37 -0400

    Part 3 done

--- lib/interpreter.ex
+++ lib/interpreter.ex
@@ -1,14 +1,8 @@
 defmodule Interpreter do
   @moduledoc false
-  alias Interpreter.{Token}
+  alias Interpreter.{Lexer, Token}
 
   defstruct text: nil, pos: 0, current_token: nil, current_char: nil
-
-  defmacro is_digit(s) do
-    quote do
-      unquote(s) in ~w(0 1 2 3 4 5 6 7 8 9)
-    end
-  end
 
   def interpreter(input) do
     trimmed = String.trim input
@@ -16,80 +10,34 @@ defmodule Interpreter do
     %Interpreter{text: trimmed, current_char: first_char}
   end
 
-  def expr(interp) do
-    interp = get_next_token interp
+  def expr(interp, result \\ 0)
+  def expr(%{current_token: %{type: :eof}}, result) do
+    result
+  end
+  def expr(%{current_token: %{type: :plus}} = interp, result) do
+    interp = eat interp, :plus
+    {res, interp} = term interp
+    expr(interp, result + res)
+  end
+  def expr(%{current_token: %{type: :minus}} = interp, result) do
+    interp = eat interp, :minus
+    {res, interp} = term interp
+    expr(interp, result - res)
+  end
+  def expr(interp, 0) do
+    interp = Lexer.get_next_token interp
 
-    left = interp.current_token
-    interp = eat interp, :integer
+    {result, interp} = term interp
+    expr interp, result
+  end
 
-    op = interp.current_token
-    interp = eat interp, op.type
-
-    right = interp.current_token
-    %{current_token: %{type: :eof, value: nil}} = eat interp, :integer
-
-    if op.type == :plus do
-      left.value + right.value
-    else
-      left.value - right.value
-    end
+  defp term(%Interpreter{current_token: token} = interp) do
+    {token.value, eat(interp, :integer)}
   end
 
   defp eat(%Interpreter{current_token: %Token{type: type}} = interp, tok_type)
     when type == tok_type and is_atom(tok_type) do
 
-    get_next_token interp
-  end
-
-  defp get_next_token(%Interpreter{current_char: nil} = interp) do
-    %{interp | current_token: Token.token(:eof, nil)}
-  end
-  defp get_next_token(%Interpreter{current_char: " "} = interp) do
-    interp
-    |> skip_whitespace()
-    |> get_next_token()
-  end
-  defp get_next_token(%Interpreter{current_char: c} = interp)
-    when is_digit(c) do
-
-    integer interp
-  end
-  defp get_next_token(%Interpreter{current_char: "+"} = interp) do
-    interp
-    |> advance()
-    |> (fn(i) -> %{i | current_token: Token.token(:plus, "+")} end).()
-  end
-  defp get_next_token(%Interpreter{current_char: "-"} = interp) do
-    interp
-    |> advance()
-    |> (fn(i) -> %{i | current_token: Token.token(:minus, "-")} end).()
-  end
-
-  defp integer(interp, int_res \\ "")
-  defp integer(%Interpreter{current_char: c} = interp, int_res)
-    when is_digit(c) do
-
-    interp
-    |> advance()
-    |> integer(int_res <> c)
-  end
-  defp integer(interp, int_res) do
-    int_val = String.to_integer int_res
-    %{interp | current_token: Token.token(:integer, int_val)}
-  end
-
-  defp skip_whitespace(%Interpreter{current_char: " "} = interp) do
-    interp
-    |> advance()
-    |> skip_whitespace()
-  end
-  defp skip_whitespace(interp), do: interp
-
-  defp advance(%Interpreter{text: text, pos: pos} = interp) do
-    new_pos = pos + 1
-    case String.at text, new_pos do
-      nil -> %{interp | current_char: nil}
-      c -> %{interp | pos: new_pos, current_char: c}
-    end
+    Lexer.get_next_token interp
   end
 end
---
+++ lib/interpreter/lexer.ex
@@ -0,0 +1,63 @@
+defmodule Interpreter.Lexer do
+  @moduledoc false
+
+  alias Interpreter.Token
+
+  defmacro is_digit(s) do
+    quote do
+      unquote(s) in ~w(0 1 2 3 4 5 6 7 8 9)
+    end
+  end
+
+  def get_next_token(%Interpreter{current_char: nil} = interp) do
+    %{interp | current_token: Token.token(:eof, nil)}
+  end
+  def get_next_token(%Interpreter{current_char: " "} = interp) do
+    interp
+    |> skip_whitespace()
+    |> get_next_token()
+  end
+  def get_next_token(%Interpreter{current_char: c} = interp)
+    when is_digit(c) do
+
+    integer interp
+  end
+  def get_next_token(%Interpreter{current_char: "+"} = interp) do
+    interp
+    |> advance()
+    |> (fn(i) -> %{i | current_token: Token.token(:plus, "+")} end).()
+  end
+  def get_next_token(%Interpreter{current_char: "-"} = interp) do
+    interp
+    |> advance()
+    |> (fn(i) -> %{i | current_token: Token.token(:minus, "-")} end).()
+  end
+
+  defp integer(interp, int_res \\ "")
+  defp integer(%Interpreter{current_char: c} = interp, int_res)
+    when is_digit(c) do
+
+    interp
+    |> advance()
+    |> integer(int_res <> c)
+  end
+  defp integer(interp, int_res) do
+    int_val = String.to_integer int_res
+    %{interp | current_token: Token.token(:integer, int_val)}
+  end
+
+  defp skip_whitespace(%Interpreter{current_char: " "} = interp) do
+    interp
+    |> advance()
+    |> skip_whitespace()
+  end
+  defp skip_whitespace(interp), do: interp
+
+  defp advance(%Interpreter{text: text, pos: pos} = interp) do
+    new_pos = pos + 1
+    case String.at text, new_pos do
+      nil -> %{interp | current_char: nil}
+      c -> %{interp | pos: new_pos, current_char: c}
+    end
+  end
+end
